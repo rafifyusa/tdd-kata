b253b800e74dcf5883d12584f34c9483165471b0
Javier Chacana <javierchacana@gmail.com>
2018-10-07 21:28:39 +0800

    First commit

---
+++ src/main/java/com/mitrais/tdd/kata/Main.java
@@ -0,0 +1,11 @@
+package com.mitrais.tdd.kata;
+
+/**
+ * Created by jchacana on 10/1/18.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Main class");
+    }
+}
