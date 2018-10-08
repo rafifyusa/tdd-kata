532e2953c5fb1dde150708127c621951fec8de07
Septian Baskoro Adhi <SeptianBaskoro.Adhi@mitrais.com>
2018-10-08 15:28:14 +0700

    fix the add method with custom delimiters

--- src/main/java/com/mitrais/tdd/kata/Calculator.java
+++ src/main/java/com/mitrais/tdd/kata/Calculator.java
@@ -6,11 +6,21 @@ package com.mitrais.tdd.kata;
 public class Calculator {
 
     public int add(String input) {
-        if (!input.isEmpty()) { 
-            String[] listS = input.split("[,\n]");            
+        if (!input.isEmpty()) {
             int rtn = 0;
-            for(String s : listS) {
-                rtn += Integer.parseInt(s);
+            if (input.startsWith("//")) {
+                Character delimiter = input.charAt(2);
+                String txt = input.substring(4);
+                System.out.println(txt);
+                String[] listS = txt.split("["+delimiter+"\n]");
+                for (String s : listS) {
+                    rtn += Integer.parseInt(s);
+                }
+            } else {
+                String[] listS = input.split("[,\n]");
+                for (String s : listS) {
+                    rtn += Integer.parseInt(s);
+                }
             }
             return rtn;
         } else {
