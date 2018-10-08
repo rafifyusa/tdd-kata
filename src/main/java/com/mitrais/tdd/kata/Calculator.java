e5418cfee1b7a326b6105bbcf0257b5a6946a17f
Andrya_A354 <dddd@gmail.com>
2018-10-08 15:37:54 +0700

    commit andrya

--- src/main/java/com/mitrais/tdd/kata/Calculator.java
+++ src/main/java/com/mitrais/tdd/kata/Calculator.java
@@ -8,20 +8,20 @@ public class Calculator {
     public int add(String input) {
         if (!input.isEmpty()) {
             int rtn = 0;
+            String delimiter =",";
+
+
             if (input.startsWith("//")) {
-                Character delimiter = input.charAt(2);
-                String txt = input.substring(4);
-                System.out.println(txt);
-                String[] listS = txt.split("["+delimiter+"\n]");
-                for (String s : listS) {
-                    rtn += Integer.parseInt(s);
-                }
-            } else {
-                String[] listS = input.split("[,\n]");
-                for (String s : listS) {
-                    rtn += Integer.parseInt(s);
-                }
+                delimiter = String.valueOf(input.charAt(2));
+                input = input.substring(4);
+
             }
+
+            String[] listS = input.split("["+delimiter+"\n]");
+            for (String s : listS) {
+                rtn += Integer.parseInt(s);
+            }
+
             return rtn;
         } else {
             return 0;
