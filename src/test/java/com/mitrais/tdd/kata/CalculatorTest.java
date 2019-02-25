454669336b39bedbb2ca657b2b1927be30d99ab3
Javier Chacana <javierchacana@gmail.com>
2019-02-25 14:19:52 +0800

    Created Calculator class and passed first test

---
+++ src/test/java/com/mitrais/tdd/kata/CalculatorTest.java
@@ -0,0 +1,16 @@
+package com.mitrais.tdd.kata;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by jchacana on 2/25/19.
+ */
+public class CalculatorTest {
+
+    @Test
+    public void testEmptyCase() {
+        Calculator calculator = new Calculator();
+        Assert.assertEquals(0, calculator.add(""));
+    }
+}
