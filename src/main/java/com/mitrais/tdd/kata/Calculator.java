2872f55ad432fd2324c9d57513fa03ff0042a8dd
Javier Chacana <javierchacana@gmail.com>
2019-07-26 13:14:07 +0800

    First couple of tests

---
+++ src/main/java/com/mitrais/tdd/kata/Calculator.java
@@ -0,0 +1,12 @@
+package com.mitrais.tdd.kata;
+
+/**
+ * Created by jchacana on 7/26/19.
+ */
+public class Calculator {
+
+    public int add(String s) {
+        if("".equalsIgnoreCase(s)) return 0;
+        return 1;
+    }
+}
---
+++ src/test/java/com/mitrais/tdd/kata/TestKata.java
@@ -0,0 +1,28 @@
+package com.mitrais.tdd.kata;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by jchacana on 3/26/19.
+ */
+public class TestKata {
+
+    @Test
+    public void test1() {
+        Calculator calculator = new Calculator();
+        Assert.assertEquals(0, calculator.add(""));
+    }
+
+    @Test
+    public void test2() {
+        Calculator calculator = new Calculator();
+        Assert.assertEquals(1, calculator.add("1"));
+    }
+
+    @Test
+    public void test3() {
+        Calculator calculator = new Calculator();
+        Assert.assertEquals(2, calculator.add("2"));
+    }
+}
