91b46790335b296d095a1b004812c6b318fef7d7
tripm810 <phamminhtri810@email.com>
2019-07-26 12:44:48 +0700

    Update test case 6

--- src/test/java/com/mitrais/tdd/kata/TestKata.java
+++ src/test/java/com/mitrais/tdd/kata/TestKata.java
@@ -36,5 +36,9 @@ public class TestKata {
         Calculator calculator = new Calculator();
         Assert.assertEquals(15, calculator.add("1,2,3,4,5"));
     }
-
+    @Test
+    public void test6() {
+        Calculator calculator = new Calculator();
+        Assert.assertEquals(6, calculator.add("1\n2,3"));
+    }
 }
