dd0b15f798a58f511d5d389dd075dadd249359c1
Javier Chacana <javierchacana@gmail.com>
2019-07-30 13:20:43 +0800

    First tests: Empty string and one number

---
+++ src/main/java/com/mitrais/tdd/kata/Calculator.java
@@ -0,0 +1,11 @@
+package com.mitrais.tdd.kata;
+
+/**
+ * Created by jchacana on 7/30/19.
+ */
+public class Calculator {
+    public int add(String s) {
+        if("".equalsIgnoreCase(s)) return 0;
+        return 1;
+    }
+}
---
+++ src/test/java/com/mitrais/tdd/kata/TestCalculator.java
@@ -0,0 +1,28 @@
+package com.mitrais.tdd.kata;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by jchacana on 3/26/19.
+ */
+public class TestCalculator {
+
+    @Test
+    public void testEmptyString() {
+        Calculator calculator = new Calculator();
+        Assert.assertEquals(0, calculator.add(""));
+    }
+
+    @Test
+    public void testOneNumberOne() {
+        Calculator calculator = new Calculator();
+        Assert.assertEquals(1, calculator.add("1"));
+    }
+
+    @Test
+    public void testOneNumberTwo() {
+        Calculator calculator = new Calculator();
+        Assert.assertEquals(2, calculator.add("2"));
+    }
+}
